bd506e9e8dc70b5176fc970a0e7efed3f59b7acc
ce691 <email@email.com>
2018-11-28 19:55:27 -0600

    start the book club web app

---
+++ superfan_book_manager.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+@app.route('/index')
+def index():
+    user = {'username': 'Alexis'}
+    posts = [
+        {
+            'author': {'username': 'Mason'},
+            'body': 'I loved the Harry Potter series!'
+        },
+        {
+            'author': {'username': 'Anders'},
+            'body': 'I also thought Harry Potter series was awesome!'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
+
+
+if __name__ == '__main__':
+    app.run()
