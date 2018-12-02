ff6c8d25f182100944f94cc139b31a86cac6ccb6
ce691 <email@email.com>
2018-12-01 23:25:16 -0600

    user login and registration forms

--- app/models.py
+++ app/models.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-from superfan_book_manager import db, login
+from app import db, login
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 
