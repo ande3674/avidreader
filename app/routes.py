ffdbc068f3e3bbd4bc54466cff97195830009cb7
ce691 <email@email.com>
2018-12-14 21:08:35 -0600

    can make posts

--- app/routes.py
+++ app/routes.py
@@ -2,27 +2,35 @@ from flask import render_template, flash, redirect, url_for, request
 from flask_login import current_user, login_user, logout_user, login_required
 from werkzeug.urls import url_parse
 from app import app, db
-from app.forms import LoginForm, RegistrationForm, EditProfileForm
-from app.models import User
+from app.forms import LoginForm, RegistrationForm, EditProfileForm, PostForm
+from app.models import User, Post
 from datetime import datetime
 
 
-@app.route('/')
-@app.route('/index')
+@app.route('/', methods=['GET', 'POST'])
+@app.route('/index', methods=['GET', 'POST'])
 @login_required
 def index():
-    user = {'username': 'Alexis'}
-    posts = [
-        {
-            'author': {'username': 'Mason'},
-            'body': 'I loved the Harry Potter series!'
-        },
-        {
-            'author': {'username': 'Anders'},
-            'body': 'I also thought Harry Potter series was awesome!'
-        }
-    ]
-    return render_template('index.html', title='Home Page', posts=posts)
+    form = PostForm()
+    if form.validate_on_submit():
+        post = Post(body=form.post.data, author=current_user)
+        db.session.add(post)
+        db.session.commit()
+        flash('your post is now live!')
+        return redirect(url_for('index'))
+    posts = current_user.followed_posts().all()
+    # #user = {'username': 'Alexis'}
+    # posts = [
+    #      {
+    #          'author': {'username': 'Mason'},
+    #          'body': 'I loved the Harry Potter series!'
+    #      },
+    #      {
+    #          'author': {'username': 'Anders'},
+    #          'body': 'I also thought Harry Potter series was awesome!'
+    #      }
+    # ]
+    return render_template("index.html", title='Home Page', form=form, posts=posts)
 
 
 @app.route('/login', methods=['GET', 'POST'])
