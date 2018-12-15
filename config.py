85ce5a307be64acdfb3bbae8bfddf5adf8f5be58
ce691 <email@email.com>
2018-12-14 23:28:40 -0600

    bootstrap attempt #1  -- needs fixes...

--- config.py
+++ config.py
@@ -8,3 +8,4 @@ class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
     SQL_ALCHEMY_TRACK_MODIFICATIONS = False
+    POSTS_PER_PAGE = 3
