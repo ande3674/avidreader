90322c04fec251f4a1e91512bfe6dd534ac3f433
ce691 <email@email.com>
2018-12-17 20:54:16 -0600

    add user's books to their profile page

--- app/routes.py
+++ app/routes.py
@@ -67,11 +67,9 @@ def register():
 @login_required
 def user(username):
     user = User.query.filter_by(username=username).first_or_404()
-    posts = [
-        {'author': user, 'body': 'Test post #1'},
-        {'author': user, 'body': 'Test post #2'}
-    ]
-    return render_template('user.html', user=user, posts=posts)
+    posts = user.posts.order_by(Post.timestamp.desc())
+    books = user.books
+    return render_template('user.html', user=user, posts=posts, books=books)
 
 
 @app.route('/edit_profile', methods=['GET', 'POST'])
